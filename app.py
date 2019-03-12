2ffc9ecbb3368d7aca27ffed3f53f8a59a8783a0
Kudo <kudo@example.com>
2019-03-12 15:45:07 +0900

    postについて

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import random
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -13,13 +13,23 @@ def index():
 def dice():
     result = random.randint(1, 6)
 
-    return render_template("dice.html", result=result)
+    return render_template("dice.html", yyy=result)
 
 
 @app.route("/greet/<username>", methods=["POST"])
 def greet(username):
     return render_template("greet.html", xxx=username)  # 左側のusernameは引数、右側のusernameは入力値
 
+
+@app.route("/form", methods=["GET"])
+def show_form():
+    return render_template("form.html")
+
+
+@app.route("/post_name", methods=["POST"])
+def request_post():
+    username = request.form["username"]
+    return f"こんにちは {username}さん"
 
 
 if __name__ == '__main__':
